2f3d362698734add90cf920250788b5451a7d3db
Alex Wine <alexwine36@gmail.com>
2026-02-04 08:12:55 -0700

    fix

---
+++ .changeset/sharp-phones-fall.md
@@ -0,0 +1,5 @@
+---
+"@build-qube/takeoff-calculator": patch
+---
+
+Remove unused function
--- crates/takeoff_core/src/lib.rs
+++ crates/takeoff_core/src/lib.rs
@@ -5,18 +5,3 @@ pub mod page;
 pub mod scale;
 pub mod state;
 pub mod unit;
-
-pub fn add(left: u64, right: u64) -> u64 {
-  left + right
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
-}
