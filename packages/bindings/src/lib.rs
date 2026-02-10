449a996a4e88d09c06f534e6d7e82cc4f2ca32f0
Alex Wine <alex@buildqube.com>
2026-02-10 10:58:32 -0700

    add documentation to lib

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -6,11 +6,13 @@ pub mod measurement;
 pub mod state;
 use napi_derive::napi;
 
+/// Add 100 to the input
 #[napi]
 pub fn plus_100(input: u32) -> u32 {
   input + 100
 }
 
+/// Add 200 to the input
 #[napi]
 pub fn plus_200(input: u32) -> u32 {
   input + 200
